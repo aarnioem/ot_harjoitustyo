7b2bb071f272257fa2f312091c7f7314bd23174d
aarnioem <aarnioem@gmail.com>
2023-03-28 16:22:33 +0300

    maksukortti testit

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_saldon_kasvattaminen_toimii(self):
+        self.maksukortti.lataa_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 15.00 euroa")
+
+    def test_saldo_vahenee(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_saldo_ei_riita(self):
+        self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahaa_tarpeeksi_toimii(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+
+    def test_rahaa_ei_tarpeeksi_toimii(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1500), False)
